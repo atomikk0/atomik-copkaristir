504c0a83a5edd2570df62f1e0edf2211c5713011
Atomik <138856192+atomikk0@users.noreply.github.com>
2024-01-24 21:14:08 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Atomik'
+description 'atomik-copkaristir'
+
+
+client_script 'client/client.lua'
+server_script 'server/server.lua'
+shared_script 'config.lua'

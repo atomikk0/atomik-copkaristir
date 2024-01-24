bcbea23948bb6a9c3d560afe359e2602d6c76e66
Atomik <138856192+atomikk0@users.noreply.github.com>
2024-01-24 21:13:53 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,74 @@
+Config = {}
+
+Config.MixCooldown = 1800 -- saniye cinsinden
+Config.Cooldown = 30000 -- salise cinsinden
+Config.PickTime = 10000 -- salise cinsinden
+
+Config.Items = {
+    [1] = {
+        itemName = "kaydirak",
+        minoran = 0,
+        maxoran = 1,
+        label = "Kaydırak"
+    },
+    [2] = {
+        itemName = "namlu",
+        minoran = 2,
+        maxoran = 3,
+        label = "Namlu"
+    },
+    [3] = {
+        itemName = "sarjor",
+        minoran = 4,
+        maxoran = 5,
+        label = "Şarjör"
+    },
+    [4] = {
+        itemName = "tetik",
+        minoran = 6,
+        maxoran = 7,
+        label = "Tetik"
+    },
+    [5] = {
+        itemName = "tutamac",
+        minoran = 8,
+        maxoran = 9,
+        label = "Tutamaç"
+    },
+    [6] = {
+        itemName = "yay",
+        minoran = 10,
+        maxoran = 11,
+        label = "Yay"
+    },
+    [7] = {
+        itemName = "bottle",
+        minoran = 12,
+        maxoran = 25,
+        label = "Şişe"
+    },
+    [8] = {
+        itemName = "sandwich",
+        minoran = 26,
+        maxoran = 49,
+        label = "Sandviç"
+    },
+    [9] = {
+        itemName = "water_bottle",
+        minoran = 50,
+        maxoran = 69,
+        label = "Su"
+    },
+    [10] = {
+        itemName = "coffee",
+        minoran = 70,
+        maxoran = 89,
+        label = "Kahve"
+    },
+    [11] = {
+        itemName = "kurkakola",
+        minoran = 90,
+        maxoran = 100,
+        label = "Kola"
+    },
+}
